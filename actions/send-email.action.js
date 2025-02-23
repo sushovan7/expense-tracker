4ebe743076998e836e5e4f7c1eaa12dfb1e78107
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-02-23 10:38:17 +0545

    fixed a email issue

--- actions/send-email.action.js
+++ actions/send-email.action.js
@@ -6,12 +6,11 @@ export async function sendEmail(to, subject, react) {
   const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "Finance App <onboarding@resend.dev>",
       to,
       subject,
       react,
     });
-    console.log(data);
 
     return { success: true, data };
   } catch (error) {
