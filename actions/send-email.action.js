c235a53d90a50a1b487b08e71119a42b6a5f8fb3
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-02-23 08:42:21 +0545

    update

--- actions/send-email.action.js
+++ actions/send-email.action.js
@@ -6,7 +6,7 @@ export async function sendEmail(to, subject, react) {
   const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const data = await resend.emails.send({
-      from: "Finance App <onboarding@resend.dev>",
+      from: "onboarding@resend.dev",
       to,
       subject,
       react,
